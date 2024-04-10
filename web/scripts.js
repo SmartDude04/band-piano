528696f78f813799286a09e856e3e8677416164a
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-04-09 21:54:56 -0400

    Finish landing page; adjusted for all screen sizes
    The landing page for the web app is done. It auto adjusts for screen sizes and matches the style of the other pages.

--- web/scripts.js
+++ web/scripts.js
@@ -1,3 +1,16 @@
-document.getElementById("background").addEventListener("mousemove", function() {
-    console.log("Works");
-})
+document.getElementById("main").addEventListener("mousemove", function(event) {
+    let mouseX = event.pageX;
+    let mouseY = event.pageY;
+
+    let width = event.currentTarget.offsetWidth;
+    let height = event.currentTarget.offsetHeight;
+
+    let xOffset = mouseX - width / 2;
+    let yOffset = mouseY - height / 2;
+
+    let translateFactor = 16;
+
+    document.getElementById("background").style.transform = `translate(${xOffset / translateFactor}px, ${yOffset / translateFactor}px)`;
+
+    console.log(xOffset, yOffset);
+});
