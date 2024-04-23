d39bba71895e35ca870383dc5bc14f373e96444e
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-04-23 18:18:10 -0400

    work on get-note.php and merge get-note-helper.php into get-note.php

--- web/lib/get-note.php
+++ web/lib/get-note.php
@@ -5,8 +5,8 @@ require "authentication.php";
 $conn = db_connect();
 
 // Get all notes from the database
-$notes = $conn->query("SELECT * FROM notes");
-
+// Desc because groups go from highest to lowest but higher pitches are higher values
+$notes = $conn->query("SELECT note FROM notes ORDER BY note desc");
 
 // Get an array of notes as a numerical array
 $notes = $notes->fetch_all();
@@ -17,11 +17,46 @@ if ($num_notes == 0) {
     exit();
 }
 
-require "get-note-helper.php";
+$transpositions = [
+    "flute" => 0,
+    "oboe" => 0,
+    "clarinet" => 2,
+    "alto_sax" => 9,
+    "trumpet_1" => 2,
+    "trumpet_2" => 2,
+    "trumpet_3" => 2,
+    "tenor_sax" => 2,
+    "trombone_1" => 0,
+    "trombone_2" => 0,
+    "baritone" => 0,
+    "bassoon" => 0,
+    "bari_sax" => 9,
+    "bass_clarinet" => 2,
+    "tuba" => 0,
+];
 
-// If notes are present, continue with deciding which note to display
+$bass_clef = ["tuba", "trombone_1", "trombone_2", "baritone"];
+
 session_start();
 $instrument = $_SESSION['instrument'];
+// Get the set group for the current instrument
+$groupings = $conn->query("SELECT inst_group FROM instrument_groupings WHERE inst_name = '$instrument'");
+$groupings = $groupings->fetch_all();
+$current_group = $groupings[0];
 
-// Get the groupings for the current instrument
-$groupings = $conn->query("SELECT * FROM instrument_groupings WHERE inst_name = '$instrument'");
+$note_group = ceil(((float) $current_group / 10) * $num_notes);
+
+$note = $notes[$note_group - 1][0];
+
+$note = $note + $transpositions[$instrument];
+
+// Prepare the note to be sent to the user
+if (array_search($instrument, $bass_clef)) {
+    // Lower the note to the bass clef
+    $note += 2;
+} else {
+    // Lower the note to the treble clef
+    $note += 6;
+}
+
+echo $note % 12;
