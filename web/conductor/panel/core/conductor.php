a34f47125fdd4981c9a4a99cbe9a49451627475b
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-04-26 19:44:32 -0400

    Work towards completing the conductor panels

--- web/conductor/panel/core/conductor.php
+++ web/conductor/panel/core/conductor.php
@@ -7,3 +7,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERV
 }
 
 ?>
+
+<div class="content">
+    <svg viewBox="0 0 2000 1100" id="staff" preserveAspectRatio="xMidYMid slice" class="piano">
+        <!--  Image provided by Wikimedia: https://upload.wikimedia.org/wikipedia/commons/1/15/PianoKeyboard.svg   -->
+
+        <!--  White keys   -->
+        <rect class="key-0" style="fill:white;stroke:black" x="0" y="0" width="23" height="120"/>
+        <rect class="key-2" style="fill:white;stroke:black" x="23" y="0" width="23" height="120"/>
+        <rect class="key-4" style="fill:white;stroke:black" x="46" y="0" width="23" height="120"/>
+        <rect class="key-5" style="fill:white;stroke:black" x="69" y="0" width="23" height="120"/>
+        <rect class="key-7" style="fill:white;stroke:black" x="92" y="0" width="23" height="120"/>
+        <rect class="key-9" style="fill:white;stroke:black" x="115" y="0" width="23" height="120"/>
+        <rect class="key-11" style="fill:white;stroke:black" x="138" y="0" width="23" height="120"/>
+        <!--  Black keys (overlap with the white keys)  -->
+        <rect class="key-1" style="fill:black;stroke:black" x="14.33333" y="0" width="13" height="80"/>
+        <rect class="key-3" style="fill:black;stroke:black" x="41.66666" y="0" width="13" height="80"/>
+        <rect class="key-6" style="fill:black;stroke:black" x="82.25" y="0" width="13" height="80"/>
+        <rect class="key-8" style="fill:black;stroke:black" x="108.25" y="0" width="13" height="80"/>
+        <rect class="key-10" style="fill:black;stroke:black" x="134.75" y="0" width="13" height="80"/>
+    </svg>
+</div>
