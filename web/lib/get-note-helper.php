dbeea4d7d1e444e039b702c9c802834c314622f8
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-04-25 22:08:03 -0400

    Complete work on treble clef in C. More work needed for bass clef

--- web/lib/get-note-helper.php
+++ web/lib/get-note-helper.php
@@ -29,8 +29,7 @@ function return_for_treble_c($note): string {
         $accidentals = "|false|false";
 
         // Adjust down the note for the accidentals
-        $noteChanges = 0;
-        while (!array_search($note % 12, $accidental_notes)) {
+        while (!is_int(array_search($note % 12, $accidental_notes))) {
             $note++;
             $noteChanges++;
         }
@@ -47,12 +46,88 @@ function return_for_treble_c($note): string {
         $newNote -= 12;
     }
 
-    if ($note < 60 && $note > 54) {
+    if ($note < 59 && $note > 48) {
         $newNote -= 12;
+
+        // Adjust for accidentals
         $note += $noteChanges;
         $newNote += array_search($note % 12, $accidental_notes);
+
+        // No idea why this is needed, but it works
+        $newNote += 5 - array_search($note % 12, $accidental_notes);
+
+    }
+
+    if ($note > 70 && $note < 75) {
+        $newNote += 12;
+
+        // Adjust for accidentals
+        $note += $noteChanges;
+        $newNote -= array_search($note % 12, $accidental_notes);
+
+        // No idea why this is needed, but it works
+        $newNote -= 5 - array_search($note % 12, $accidental_notes);
     }
 
     return $newNote . $accidentals;
 
+}
+
+function return_for_bass_c($note): string {
+
+    // Make the note work for treble clef in C
+    $newNote = $note;
+    $noteChanges = 0;
+
+    // Prepare the correct accidental (sharp/flat) to display with the note
+    $accidental_notes = [1, 3, 6, 8, 10];
+    if (array_search($note % 12, $accidental_notes) > -1) {
+        $newNote -= array_search($note % 12, $accidental_notes);
+        $accidentals = "|true|false";
+    } else {
+        $accidentals = "|false|false";
+
+        // Adjust down the note for the accidentals
+        while (!is_int(array_search($note % 12, $accidental_notes))) {
+            $note++;
+            $noteChanges++;
+        }
+        $newNote -= array_search($note % 12, $accidental_notes);
+
+        // Put $note back to where it was before
+        $note -= $noteChanges;
+    }
+
+    // Transposition change
+    $newNote -= 1;
+
+    $newNote -= 48;
+
+    return $newNote;
+
+    if ($note < 59 && $note > 48) {
+        $newNote -= 12;
+
+        // Adjust for accidentals
+        $note += $noteChanges;
+        $newNote += array_search($note % 12, $accidental_notes);
+
+        // No idea why this is needed, but it works
+        $newNote += 5 - array_search($note % 12, $accidental_notes);
+
+    }
+
+
+    if ($note > 70 && $note < 75) {
+        $newNote += 12;
+
+        // Adjust for accidentals
+        $note += $noteChanges;
+        // $newNote -= array_search($note % 12, $accidental_notes);
+
+        // No idea why this is needed, but it works
+//        $newNote -= 5 - array_search($note % 12, $accidental_notes);
+    }
+
+    return $newNote . $accidentals;
 }
