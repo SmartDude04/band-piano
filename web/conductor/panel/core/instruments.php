063a24e5ab5f04d015c90955e431b66ff309ef9e
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-05-01 20:15:28 -0400

    Work towards completing the instruments panel for the conductor.
    The format is done, but the PHP is not yet written. When written, it will automatically generate the code for the different groupings.

--- web/conductor/panel/core/instruments.php
+++ web/conductor/panel/core/instruments.php
@@ -7,3 +7,45 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERV
 }
 
 ?>
+
+<div class="instrumentation">
+    <div class="group">
+        <div class="sections">
+            <h1 class="group-header poppins-black">Group 1</h1>
+            <div class="instruments">
+
+                <div class="instrument">
+                    <h1 class="instrument-name poppins-bold">Trombone</h1>
+
+                    <form method="post" action="" class="buttons">
+                        <button type="submit" name="down" class="move-button">
+                            <img src="../../img/arrow-down.png" alt="down arrow">
+                        </button>
+                        <button type="submit" name="up" class="move-button">
+                            <img src="../../img/arrow-up.png" alt="down arrow">
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <?php
+
+    // Use SSR instead of SSG as the load time isn't that important
+
+    // If I was more concerned, I would send off the templates for each group then fill them in
+    // with each instrument after page load to improve load time
+
+    // Generate all groups using the SQL database
+    require "../../../lib/authentication.php";
+    $conn = db_connect();
+
+    $result = $conn->execute_query("SELECT * FROM instrument_groupings ORDER BY inst_group");
+    $result_rows = $result->fetch_all(MYSQLI_ASSOC);
+
+    
+
+    ?>
+</div>
